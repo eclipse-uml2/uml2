09ce78c574fd9607878836bdb09093320006acd9
khussey <khussey>
2006-07-25 19:28:37 +0000

    [151701] Adding missing type mapping for State::doActivity.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/UML22UMLExtendedMetaData.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/UML22UMLExtendedMetaData.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  * 
- * $Id: UML22UMLExtendedMetaData.java,v 1.3 2006/05/12 20:39:16 khussey Exp $
+ * $Id: UML22UMLExtendedMetaData.java,v 1.3.2.1 2006/07/25 19:28:37 khussey Exp $
  */
 package org.eclipse.uml2.uml.resource;
 
@@ -2415,6 +2415,7 @@ public class UML22UMLExtendedMetaData
 
 			typeMap = new HashMap();
 			typeMap.put(UMLPackage.Literals.STATE, "uml:Activity"); //$NON-NLS-1$
+			featureToTypeMap.put("doActivity", typeMap); //$NON-NLS-1$
 			featureToTypeMap.put("entry", typeMap); //$NON-NLS-1$
 			featureToTypeMap.put("exit", typeMap); //$NON-NLS-1$
 
