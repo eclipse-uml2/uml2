0577229cee7ea1a895c65e6eac6c7b9504b7c529
Kenn Hussey <kenn.hussey@gmail.com>
2017-12-28 21:28:15 -0500

    [526568] Tweaking publisher scripts.

--- releng/org.eclipse.uml2.build-feature/updates.sh
+++ releng/org.eclipse.uml2.build-feature/updates.sh
@@ -1,91 +1,60 @@
 #!/bin/bash -xv
 #
-#    Promote the PUBLISH__URL to an updates repository.
+#    Promote the PUBLISH__BUILD to an updates repository.
 #
-#    PUBLISH__URL            The zip to be published e.g. https://hudson.eclipse.org/uml2/job/mdt-uml2-master/10/artifact/releng/org.eclipse.uml2.build-feature/target/org.eclipse.uml2-5.4.0.v20171228-2349.zip
-#    PUBLISH__VERSION        Unqualified version e.g. 5.4.0
-#    PUBLISH__BUILD_T        Build type N/I/S, blank suppresses promotion
-#    PUBLISH__QUALIFIER      Version qualifier e.g. v20171228-2349
+#    PUBLISH__BUILD_TYPE     Build type N/I/S, blank suppresses promotion
+#    PUBLISH__BUILD          The base URL of the build to be published, e.g., https://hudson.eclipse.org/uml2/job/mdt-uml2-master/lastSuccessfulBuild/
+#    PUBLISH__VERSION        Unqualified version, e.g., 5.4.0
+#    PUBLISH__QUALIFIER      Version qualifier, e.g., v20171228-2349
 #
-updatesFolder="/home/data/httpd/download.eclipse.org/modeling/mdt/uml2/updates/5.4"
+updatesFolder="/home/data/httpd/download.eclipse.org/modeling/mdt/uml2/updates/"
 group="modeling.mdt.uml2"
 localZip="uml2.zip"
 projectRepoName="Unified Modeling Language 2.x (UML2)"
 manageComposite="/shared/common/apache-ant-latest/bin/ant -f /shared/modeling/tools/promotion/manage-composite.xml"
 
-if [ -n "${PUBLISH__BUILD_T}" ]
+if [ -n "${PUBLISH__BUILD_TYPE}" ]
 then
 
-  if [ "${PUBLISH__BUILD_T}" = "N" ]
+  if [ "${PUBLISH__BUILD_TYPE}" = "N" ]
   then
-    buildFolder="${updatesFolder}-N-builds"
-    buildRepoName="Nightly Build"
-  elif [ "${PUBLISH__BUILD_T}" = "I" ]
+    repoFolder="${updatesFolder}${PUBLISH__VERSION:0:2}-N-builds"
+    repoName="Nightly Build"
+  elif [ "${PUBLISH__BUILD_TYPE}" = "I" ]
   then
-    buildFolder="${updatesFolder}-I-builds"
-    buildRepoName="Integration Build"
-  elif [ "${PUBLISH__BUILD_T}" = "S" ]
+    repoFolder="${updatesFolder}${PUBLISH__VERSION:0:2}-I-builds"
+    repoName="Integration Build"
+  elif [ "${PUBLISH__BUILD_TYPE}" = "S" ]
   then
-    buildFolder="${updatesFolder}milestones"
-    buildRepoName="Milestone"
+    repoFolder="${updatesFolder}${PUBLISH__VERSION:0:2}milestones"
+    repoName="Milestone"
   else
-    buildFolder="${updatesFolder}other"
-    buildRepoName="Other"
+    repoFolder="${updatesFolder}${PUBLISH__VERSION:0:2}-other"
+    repoName="Other"
   fi
 
-  if [ ! -d "${buildFolder}" ]
+  if [ ! -d "${repoFolder}" ]
   then
-    mkdir -p ${buildFolder}
+    mkdir -p ${repoFolder}
   fi
 
-  pushd ${buildFolder}
-    if [ ! -d "${PUBLISH__VERSION}" ]
+  pushd ${repoFolder}
+
+    buildFolder="${PUBLISH__BUILD_TYPE}${PUBLISH__QUALIFIER:1:8}${PUBLISH__QUALIFIER:10:4}"
+
+    if [ ! -d "${buildFolder}" ]
     then
-      mkdir ${PUBLISH__VERSION}
-      versionCompositeName="${projectRepoName} ${buildRepoName} Site"
-      ${manageComposite} add -Dchild.repository=${PUBLISH__VERSION} -Dcomposite.name="${versionCompositeName}"
+      mkdir ${buildFolder}
     fi
 
-    if [ "${PUBLISH__BUILD_T}" = "N" ]
-    then
-      curl -s -k ${PUBLISH__URL} > ${localZip}
-      unzip -ou ${localZip} -d new${PUBLISH__VERSION}
-      chgrp -R ${group} new${PUBLISH__VERSION}
-      chmod -R g+w new${PUBLISH__VERSION}
-      mv ${PUBLISH__VERSION} old${PUBLISH__VERSION}
-      mv new${PUBLISH__VERSION} ${PUBLISH__VERSION}
-      rm -rf old${PUBLISH__VERSION} ${localZip}
-    elif [ "${PUBLISH__BUILD_T}" = "I" ]
-    then
-      curl -s -k ${PUBLISH__URL} > ${localZip}
-      unzip -ou ${localZip} -d new${PUBLISH__VERSION}
-      chgrp -R ${group} new${PUBLISH__VERSION}
-      chmod -R g+w new${PUBLISH__VERSION}
-      mv ${PUBLISH__VERSION} old${PUBLISH__VERSION}
-      mv new${PUBLISH__VERSION} ${PUBLISH__VERSION}
-      rm -rf old${PUBLISH__VERSION} ${localZip}
-    elif [ "${PUBLISH__BUILD_T}" = "S" ]
-    then
-      pushd ${buildFolder}/${PUBLISH__VERSION}
+    curl -s -k ${PUBLISH__BUILD}/artifact/releng/org.eclipse.uml2.build-feature/target/org.eclipse.uml2-${PUBLISH__VERSION}.${PUBLISH__QUALIFIER}.zip > ${localZip}
+    unzip ${localZip} -d ${buildFolder}
+    rm ${localZip}
 
-        tQualifier="${PUBLISH__BUILD_T}${PUBLISH__QUALIFIER:1:8}${PUBLISH__QUALIFIER:10:4}"
-        versionFolder="${buildFolder}/${tQualifier}"
-        if [ ! -d "${tQualifier}" ]
-        then
-          mkdir ${tQualifier}
-        fi
+    chgrp -R ${group} ${buildFolder}
+    chmod -R g+w ${buildFolder}
+    ${manageComposite} add -Dchild.repository=${buildFolder} -Dcomposite.name="${projectRepoName} ${PUBLISH__VERSION} ${repoName} Site"
 
-        curl -s -k ${PUBLISH__URL} > ${localZip}
-        unzip ${localZip} -d ${tQualifier}
-        rm ${localZip}
-
-        chgrp -R ${group} ${tQualifier}
-        chmod -R g+w ${tQualifier}
-        ${manageComposite} add -Dchild.repository=${tQualifier} -Dcomposite.name="${projectRepoName} ${PUBLISH__VERSION} ${buildRepoName} Site"
-
-      popd
-    fi
-   
   popd
 
 fi
