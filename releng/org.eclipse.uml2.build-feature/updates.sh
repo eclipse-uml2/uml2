2bc3ad5f019beb85fb47aaebb709bc0d27d78e2c
Kenn Hussey <kenn.hussey@gmail.com>
2018-02-05 13:16:20 -0500

    [530650] Updating based on migration to Jenkins.

--- releng/org.eclipse.uml2.build-feature/updates.sh
+++ releng/org.eclipse.uml2.build-feature/updates.sh
@@ -3,7 +3,7 @@
 #    Promote the PUBLISH__BUILD to an updates repository.
 #
 #    PUBLISH__BUILD_TYPE     Build type N/I/S/R, blank suppresses promotion
-#    PUBLISH__BUILD          The base URL of the build to be published, e.g., https://hudson.eclipse.org/uml2/job/mdt-uml2-master/lastSuccessfulBuild
+#    PUBLISH__BUILD          The base URL of the build to be published, e.g., https://ci.eclipse.org/uml2/job/mdt-uml2-master/lastSuccessfulBuild
 #    PUBLISH__VERSION        Unqualified version, e.g., 5.4.0
 #    PUBLISH__QUALIFIER      Build qualifier, e.g., I20171228-2349
 #
