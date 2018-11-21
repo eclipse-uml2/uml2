bfc3b694a163d0cfd9fe56d8ddbf0d3df16de580
Kenn Hussey <kenn.hussey@gmail.com>
2018-11-21 17:08:32 -0500

    [535301] Updating org.eclipse.uml2.codegen.ecore.ui to EPL v2.0.

--- plugins/org.eclipse.uml2.codegen.ecore.ui/src/org/eclipse/uml2/codegen/ecore/genmodel/provider/GenModelItemProviderAdapterFactory.java
+++ plugins/org.eclipse.uml2.codegen.ecore.ui/src/org/eclipse/uml2/codegen/ecore/genmodel/provider/GenModelItemProviderAdapterFactory.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2005, 2008 IBM Corporation, Embarcadero Technologies, and others.
+ * Copyright (c) 2005, 2018 IBM Corporation, Embarcadero Technologies, and others.
  * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  *   IBM - initial API and implementation
  *   Kenn Hussey (Embarcadero Technologies) - 247980
+ *   Kenn Hussey - 535301
  *
- * $Id: GenModelItemProviderAdapterFactory.java,v 1.5 2008/12/16 15:52:39 khussey Exp $
  */
 package org.eclipse.uml2.codegen.ecore.genmodel.provider;
 
