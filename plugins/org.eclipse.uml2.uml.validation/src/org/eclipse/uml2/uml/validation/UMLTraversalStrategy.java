f04794c76066f4b7943e90b56f436d3985b112b9
Kenn Hussey <kenn.hussey@gmail.com>
2018-11-22 14:26:06 -0500

    [535301] Updating org.eclipse.uml2.uml.validation to EPL v2.0.

--- plugins/org.eclipse.uml2.uml.validation/src/org/eclipse/uml2/uml/validation/UMLTraversalStrategy.java
+++ plugins/org.eclipse.uml2.uml.validation/src/org/eclipse/uml2/uml/validation/UMLTraversalStrategy.java
@@ -1,13 +1,14 @@
 /*
- * Copyright (c) 2013, 2014 CEA LIST and others.
+ * Copyright (c) 2013, 2018 CEA LIST and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  *     E.D.Willink (CEA LIST) - initial API and implementation
  *     Christian W. Damus (CEA) - 397508
+ *     Kenn Hussey - 535301
  */
 package org.eclipse.uml2.uml.validation;
 
